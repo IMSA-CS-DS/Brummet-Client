a87318165d7e3ed81f45bb522f1d60e6b48873c7
ltcptgeneral <35508619+ltcptgeneral@users.noreply.github.com>
2019-04-11 16:43:46 -0500

    working kinda
    need to figure out how to pass variables into different class method

---
+++ main.py
@@ -0,0 +1,80 @@
+from kivy.config import Config
+Config.set('graphics', 'resizable', False)
+
+from kivy.app import App
+
+from kivy.properties import StringProperty
+
+from kivy.core.window import Window
+
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+
+import csv
+import paramiko
+#import os
+
+def load_csv(filepath):
+    with open(filepath, newline='') as csvfile:
+        file_array = list(csv.reader(csvfile))
+        csvfile.close()
+    return file_array
+
+class Connect(Screen):
+    Window.size = (600, 300)
+    #self.manager.get_screen('connecting')
+    def on_enter(self, username, password):
+
+        host = 'titanrobotics.ddns.net'
+        port = 60022
+        print(username, password)
+        self.ids.status.text = "connecting"
+        
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        try:
+            self.ids.status.text = "attempting to connect to " + host
+            ssh.connect(host, port, username, password)
+            yield ssh
+            self.ids.status.text = "connected to " + host
+
+        finally:
+            ssh.close()
+            self.ids.status.text = "connection failed"
+
+        #print("here")
+    #ssh = loginroutine(username, password)
+
+class Login(Screen):
+    Window.size = (600, 300)
+    def do_login(self, loginText, passwordText):
+        app = App.get_running_app()
+
+        username = loginText
+        password = passwordText
+
+        #print(username, password)
+        #self.manager.get_screen('connecting').loginroutine(username, password)
+        #self.manager.current_screen.loginroutine(username, password)
+        #print("here")
+
+    def resetForm(self):
+        self.ids['login'].text = ""
+        self.ids['password'].text = ""
+
+class BrummetApp(App):
+    username = StringProperty(None)
+    password = StringProperty(None)
+
+    title = 'Brummet Client v ' + load_csv("data/meta")[0][1]
+
+    def build(self):
+        manager = ScreenManager()
+
+        manager.add_widget(Login(name = 'login'))
+        manager.add_widget(Connect(name = 'connecting'))
+
+        return manager
+
+if __name__ == '__main__':
+    BrummetApp().run()

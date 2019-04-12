0bc181f7f9a01b60d360c79328d7a73e7d6c207e
ltcptgeneral <35508619+ltcptgeneral@users.noreply.github.com>
2019-04-12 14:09:40 -0500

    working

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+# IMSA Computational Science and Data Science Club: Brummet Clinet
+# Written for ssh into IMSA SLURM cluster
+# Written by: Arthur Lu, Jacob Levine
+# Use at one's own risk
+
+__author__ = ("Arthur Lu <alu1@imsa.edu>", "Jacob Levine <jlevine@imsa.edu>")
+
 from kivy.config import Config
 Config.set('graphics', 'resizable', False)
 
 from kivy.app import App
 
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 
 from kivy.core.window import Window
 
-from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition, NoTransition
 
 import csv
 import paramiko
@@ -16,46 +23,55 @@ import paramiko
 def load_csv(filepath):
     with open(filepath, newline='') as csvfile:
         file_array = list(csv.reader(csvfile))
-        csvfile.close()
+        csvfile.close() 
     return file_array
 
 class Connect(Screen):
-    Window.size = (600, 300)
-    def routine(self, username, password):
+    Window.size = (600, 350)
+    def routine(self, host, port, username, password):
 
-        host = 'titanrobotics.ddns.net'
-        port = 60022
-        print(username, password)
+        #print(username, password)
         self.ids.status.text = "connecting"
         
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
+        sftp = None
+
         try:
-            self.ids.status.text = "attempting to connect to " + host
+            self.ids.status.text = "attempting to connect to " + host + ":" + str(port)
             ssh.connect(host, port, username, password)
-            #yield ssh
-            self.ids.status.text = "connected to " + host
+            self.ids.status.text = "connected to " + host + ":" + str(port)
 
-        except:
+            transport = paramiko.Transport((host, port))
+            transport.connect(username = username, password = password)
+            sftp = paramiko.SFTPClient.from_transport(transport)
+
+        except Exception as e:
             ssh.close()
-            self.ids.status.text = "connection failed"
-
-        #print("here")
-    #ssh = loginroutine(username, password)
+            self.ids.status.text = "connection failed: " + str(e)
 
 class Login(Screen):
     Window.size = (600, 300)
-    def do_login(self, loginText, passwordText):
+
+    def do_login(self, loginText, passwordText, hostText, portText):
         app = App.get_running_app()
 
+        if hostText == "":
+            hostText = "slurm.imsa.edu"
+        if portText == "":
+            portText = "22"
+
+        host = hostText
+        port = int(portText)
+        
         username = loginText
         password = passwordText
 
         self.manager.transition = SlideTransition(direction = "left")
         self.manager.current = "connect"
 
-        self.manager.get_screen('connect').routine(username, password)
+        self.manager.get_screen('connect').routine(host, port, username, password)
 
     def resetForm(self):
         self.ids['login'].text = ""

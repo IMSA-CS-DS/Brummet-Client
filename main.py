675e37294ba5eca30799946ecdbe6ab7b1005d34
ltcptgeneral <35508619+ltcptgeneral@users.noreply.github.com>
2019-04-15 09:52:19 -0500

    Update main.py
    semi not working

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# IMSA Computational Science and Data Science Club: Brummet Clinet
+# IMSA Computational Science and Data Science Club: Brummet Client
 # Written for ssh into IMSA SLURM cluster
 # Written by: Arthur Lu, Jacob Levine
 # Use at one's own risk
@@ -30,7 +30,7 @@ def load_csv(filepath):
     return file_array
 
 class Client(Screen):
-	Window.size = (1280, 720)
+    Window.size = (1280, 720)
 
 class Connect(Screen):
     Window.size = (600, 300)
@@ -39,31 +39,42 @@ class Connect(Screen):
 
         #print(username, password)
         self.ids.status.text = "connecting"
-        
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
+               
+        ssh = None
         sftp = None
 
         try:
             self.ids.status.text = "attempting to connect to " + host + ":" + str(port)
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             ssh.connect(host, port, username, password)
-            self.ids.status.text = "connected to " + host + ":" + str(port)
-
+            
             transport = paramiko.Transport((host, port))
             transport.connect(username = username, password = password)
             sftp = paramiko.SFTPClient.from_transport(transport)
 
+            self.ids.status.text = "connected to " + host + ":" + str(port)
+
+            Clock.schedule_once(self.continue_to_client, 2)
+
         except Exception as e:
-            ssh.close()
+            if sftp is not None:
+                sftp.close()
+            if ssh is not None:
+                ssh.close()
+
             self.ids.status.text = "connection failed: " + str(e)
-            Clock.schedule_once(self.return_to_login, 2.5)
+            Clock.schedule_once(self.return_to_login, 2)
             #self.manager.current = 'login'
 
     def return_to_login(self, *args):
-    	self.manager.transition = SlideTransition(direction = "right")
-    	self.manager.current = 'login'
+        self.manager.transition = SlideTransition(direction = "right")
+        self.manager.current = 'login'
             #time.sleep(5)
+
+    def continue_to_client(self, *args):
+        self.manager.transition = NoTransition()
+        self.manager.current = 'client'
             
 
 class Login(Screen):
@@ -101,12 +112,14 @@ class BrummetApp(App):
     username = StringProperty(None)
     password = StringProperty(None)
 
-    
+    screenName = StringProperty(None)
 
     title = 'Brummet Client v ' + load_csv("data/meta")[0][1]
 
     def check_resize(self, instance, x, y):
         # resize X
+        screenName = manager.current
+        print(screenName)
         if manager.current != "client":
 
             if x >  target_x:
@@ -123,13 +136,20 @@ class BrummetApp(App):
 
     def build(self):
 
-        Window.bind(on_resize=self.check_resize)
-
         manager = ScreenManager()
+        loginScr = Screen(name = 'login')
+        loginScr.add_widget(Login())
+        manager.add_widget(loginScr)
 
-        manager.add_widget(Login(name = 'login'))
-        manager.add_widget(Connect(name = 'connect'))
-        manager.add_widget(Client(name = 'client'))
+        connectScr = Screen(name = 'connect')
+        connectScr.add_widget(Connect())
+        manager.add_widget(connectScr)
+
+        clientScr = Screen(name = 'client')
+        clientScr.add_widget(Client())
+        manager.add_widget(clientScr)
+
+        Window.bind(on_resize=self.check_resize)
 
         return manager
 
